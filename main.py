42b852fa5938ade52454072a61748696e01c368b
georghohn83-art <georghohn83@gmail.com>
2026-05-05 00:15:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,15 @@ import os
 
 intents = discord.Intents.default()
 intents.message_content = True
-intents.members = True
-intents.presences = True
 
 client = commands.Bot(command_prefix="!", intents=intents)
 
 @client.event
 async def on_ready():
     print(f'Bot ist online als {client.user}')
-    print('------')
 
 @client.command()
 async def ping(ctx):
-    await ctx.send("Pong! Ich lebe.")
+    await ctx.send("Pong! Ich lebe endlich.")
 
 client.run(os.getenv("DISCORD_TOKEN"))
