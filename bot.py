64fc39e32b3328aaf24d6b2d2a2c55293f6355a9
georghohn83-art <georghohn83@gmail.com>
2026-05-04 20:13:49 +0200

    Refactor bot commands and update trigger phrase

--- bot.py
+++ bot.py
@@ -1,12 +1,20 @@
 import discord
 import os
+
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
+
 @client.event
-async def on_ready(): print('Phoenix online')
+async def on_ready():
+    print('Phoenix online')
+
 @client.event
-async def on_message(m):
- if m.author == client.user: return
- if m.content == '!phoenix': await m.channel.send('Ritter Georg, Galaxia lebt.')
-client.run(os.getenv('TOKEN'))
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content == '!phönix':
+        await message.channel.send('Ritter Georg, Galaxia lebt!')
+
+client.run(os.getenv("TOKEN"))
+
