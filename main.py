81c715d8fc805435196d3fee9a4eaba39892373d
georghohn83-art <georghohn83@gmail.com>
2026-05-04 22:26:43 +0200

    Add basic Discord bot with HTTP server

---
+++ main.py
@@ -0,0 +1,8 @@
+import os,threading,discord
+from http.server import *
+class H(BaseHTTPRequestHandler):def do_GET(s):s.send_response(200);s.end_headers()
+threading.Thread(target=lambda:HTTPServer(('0.0.0.0',int(os.getenv('PORT',10000))),H).serve_forever(),daemon=1).start()
+bot=discord.Client(intents=discord.Intents.all())
+@bot.event
+async def on_ready():print("Bot laeuft")
+bot.run(os.getenv('DISCORD_TOKEN'))
