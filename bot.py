60516fe63421b3666cc477621aac8f558153804c
georghohn83-art <georghohn83@gmail.com>
2026-05-04 23:28:43 +0200

    Update Discord bot intents and token variable

--- bot.py
+++ bot.py
@@ -3,12 +3,14 @@ import os
 
 intents = discord.Intents.default()
 intents.message_content = True
-client = discord.Client(intents=intents)
+intents.members = True
+intents.presences = True
 
-token = os.getenv("DISCORD_TOKEN")
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
     print(f'Bot ist online als {client.user}')
+    print(f'Shard ID 0 hat sich mit dem Gateway verbunden')
 
-client.run(token)
+client.run(os.getenv("DISCORD_BOT_TOKEN"))
