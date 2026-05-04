94b4ad8023010d54533f333d8110b45ac58acf6c
georghohn83-art <georghohn83@gmail.com>
2026-05-04 22:56:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import os,threading,discord
-from http.server import *
-class H(BaseHTTPRequestHandler):def do_GET(self):s.send_response(200);s.end_headers()
-threading.Thread(target=lambda:HTTPServer(('0.0.0.0',int(os.getenv('PORT',10000))),H).serve_forever(),daemon=1).start()
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class H(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'Bot is running')
+
+threading.Thread(target=lambda:HTTPServer(('0.0.0.0',int(os.getenv('PORT',10000))),H).serve_forever(),daemon=True).start()
+
 bot=discord.Client(intents=discord.Intents.all())
+
 @bot.event
-async def on_ready():print("Bot laeuft")
+async def on_ready():
+    print("Bot laeuft")
+
 bot.run(os.getenv('DISCORD_TOKEN'))
