d44abb3d241a27d9d28f7205a1352b76a786b9bc
georghohn83-art <georghohn83@gmail.com>
2026-05-04 21:34:02 +0200

    Refactor bot to use client instead of bot

--- bot.py
+++ bot.py
@@ -1,26 +1,15 @@
-import discord
+
+    import discord
 import os
-import sys
-
-print("1. Script startet")
-TOKEN = os.getenv("TOKEN")
-print("2. Token da:", bool(TOKEN))
-
-if not TOKEN:
-    print("FEHLER: TOKEN fehlt komplett")
-    sys.exit(1)
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = discord.Client(intents=intents)
+client = discord.Client(intents=intents)
 
-@bot.event
+token = os.getenv("DISCORD_TOKEN")
+
+@client.event
 async def on_ready():
-    print(f"PHOENIX ONLINE ALS {bot.user}")
+    print(f'Bot ist online als {client.user}')
 
-print("3. Vor bot.run")
-try:
-    bot.run(TOKEN)
-except Exception as e:
-    print("FEHLER BEIM LOGIN:", e)
-    sys.exit(1)
+client.run(token)
